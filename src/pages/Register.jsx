c33a9929d00021cc52b33cfff0a58167d2e484d8
danyamasloff <danyamasloff@yandex.ru>
2025-03-24 16:15:45 +0300

    Полное обновление проекта: добавлены профиль, каталог, анимации и исправлена аутентификация

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Box, Container, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
 import RegisterForm from '../components/auth/RegisterForm';
+import { AnimatedBox, pageTransition } from '../components/ui/AnimatedComponents';
 import { ROUTES } from '../utils/constants';
 
 const Register = () => {
@@ -18,20 +20,36 @@ const Register = () => {
 
     return (
         <Container maxWidth="md">
-            <Box sx={{ py: 4 }}>
-                <Typography
-                    variant="h4"
-                    component="h1"
-                    textAlign="center"
-                    gutterBottom
-                    fontFamily="Ubuntu"
-                    fontWeight={700}
-                    color="primary.main"
+            <AnimatedBox
+                sx={{ py: 4 }}
+                {...pageTransition}
+            >
+                <motion.div
+                    initial={{ y: -20, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.5 }}
                 >
-                    Библиотека ИУЦТ ЦТУТП
-                </Typography>
-                <RegisterForm />
-            </Box>
+                    <Typography
+                        variant="h4"
+                        component="h1"
+                        textAlign="center"
+                        gutterBottom
+                        fontFamily="Ubuntu"
+                        fontWeight={700}
+                        color="primary.main"
+                    >
+                        Библиотека ИУЦТ ЦТУТП
+                    </Typography>
+                </motion.div>
+
+                <motion.div
+                    initial={{ y: 20, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.2 }}
+                >
+                    <RegisterForm />
+                </motion.div>
+            </AnimatedBox>
         </Container>
     );
 };
